c5f11966fc987e4ac3040171a41245b2d2b050a0
KasiaHajduk <kasiahajduk2014@gmail.com>
2022-03-27 13:10:22 +0200

    1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import { App } from 'components/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+        </Route>
+      </Routes>
+
+
+
+    </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
