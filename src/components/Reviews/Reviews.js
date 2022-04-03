3b86cd61a2797291df41cfc5aa6df1d107e005c8
KasiaHajduk <kasiahajduk2014@gmail.com>
2022-04-03 17:14:17 +0200

    1714

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -35,17 +35,19 @@ export default function Reviews() {
 
 
     return (
-        <ul>
+        <div>
             {reviews.length > 0 ? (
-                reviews.map (({id, author, content}) => (
+            <ul>
+                {reviews.map(({ id, author, content }) => (
                     <li key={id}>
                         <h2>Author: {author}</h2>
                         <p>{content}</p>
                     </li>
-                ))
+                ))}
+            </ul>
             ) : (
                 <p>We don't have any reviews for this movie.</p>
             )}
-        </ul>
+        </div>
     )
 }
